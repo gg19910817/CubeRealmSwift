04fb13d5c59a8a6185b89ae0156d8b2468ef66f5
gg19910817 <gg19910817@gmail.com>
2020-05-15 09:40:55 +0800

    测试数据

--- Example/CubeRealmSwift/RealmObjects.swift
+++ Example/CubeRealmSwift/RealmObjects.swift
@@ -9,11 +9,21 @@
 import UIKit
 import RealmSwift
 
+/*
+ Tips: Realm
+ 1. 所有定义的Object在第一次读取数据库的时候会全部加载，所以过多会导致性能问题。
+ */
+
 // 群组表
 class Group: Object {
     @objc dynamic var id: String?
     @objc dynamic var name: String?
     let users = List<GroupUser>()
+    
+    // 主键
+    override static func primaryKey() -> String? {
+        return "id"
+    }
 }
 
 // 群组用户表 0:普通用户 1:管理员 2:群主
@@ -29,6 +39,11 @@ class User: Object {
     @objc dynamic var id: String?
     @objc dynamic var name: String?
     @objc dynamic var avatar: String?
+    
+    // 主键
+    override static func primaryKey() -> String? {
+        return "id"
+    }
 }
 
 // 好友关系
@@ -36,20 +51,30 @@ class FriendShip: Object {
     @objc dynamic var user: User?
 }
 
-// 会话，包含 0:单聊 1:群聊 2:
+// 会话，包含 0:单聊 1:群聊
 class Conversation: Object {
     @objc dynamic var type = 0
+    @objc dynamic var group: Group?
+    @objc dynamic var receiver: User?
     let messages = List<Message>()
 }
 
 // 消息，一条消息可能存在多个Elem
 class Message: Object {
     @objc dynamic var sender: User?
+    @objc dynamic var date: Date?
     let elems = List<MessageElem>()
 }
 
-// 消息的基本元素，例如文本，文件（图片，语音）。
+// 消息的基本元素，例如文本，文件，图片，语音。
 class MessageElem: Object {
     @objc dynamic var type = 0
-    @objc dynamic var data: Data?
+    // 文本消息此处为文本，其他消息为Url
+    @objc dynamic var content: String?
+    /*
+     Tips: Realm双向关联
+     1. 建立Message和Elem的双向关联，比在这里定义一个message更有意义。
+     2. 这里能获得所有elems中包含此Elem的Message，所以是一个数组。
+     */
+//    let messages = LinkingObjects(fromType: Message.self, property: "elems")
 }
--- Example/CubeRealmSwift/ViewController.swift
+++ Example/CubeRealmSwift/ViewController.swift
@@ -7,17 +7,114 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mockUser()
+        mockGroup()
+        mockUserMessage()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func mockGroup() {
+        // Get the default Realm
+        let realm = try! Realm()
+        
+        let groups = List<Group>()
+        for i in 1...1000 {
+            let group = Group()
+            group.id = String(10000 + i)
+            group.name = "Group\(10000 + i)"
+            let users = group.users
+            for j in 1...8000 {
+                let user: User? = realm.object(ofType: User.self, forPrimaryKey: String(1000000 + j))
+                let groupUser = GroupUser()
+                groupUser.user = user
+                groupUser.avatar = "http://www.cs999.com/avatar/\(user!.id!)"
+                groupUser.name = "GroupUserName"
+                users.append(groupUser)
+            }
+            groups.append(group)
+        }
+        
+        // Persist your data easily
+        try! realm.write {
+            realm.add(groups)
+        }
+    }
+    
+    func mockUser() {
+        let users = List<User>()
+        
+        for i in 1...100000 {
+            let user = User()
+            user.id = String(1000000 + i)
+            user.name = "user\(user.id!)"
+            user.avatar = "http://www.cs999.com/avatar/\(user.id!)"
+            users.append(user)
+        }
+        
+        // Get the default Realm
+        let realm = try! Realm()
+        // Persist your data easily
+        try! realm.write {
+            realm.add(users)
+        }
+    }
+    
+    func mockUserMessage() {
+        
+        // Get the default Realm
+        let realm = try! Realm()
+        
+        let list = List<Conversation>()
+        
+        for i in 1...10 {
+            let conversation = Conversation()
+            let user: User? = realm.object(ofType: User.self, forPrimaryKey: String(1000000 + 8000*i + i))
+            conversation.receiver = user
+            let messages = conversation.messages
+            for j in 1...20000 {
+                let message = Message()
+                message.sender = user
+                message.date = Date()
+                let elems = message.elems
+                for k in 1...3 {
+                    let elem = MessageElem()
+                    elem.type = k%3
+                    if elem.type == 0 {
+                        elem.content = "/Users/gg19910817/Library/Developer/CoreSimulator/Devices/50AEB5D7-785C-41C1-9198-66767DE93CEE/data/Containers/Data/Application/1076CA45-E5AC-4DDD-8240-0827A24DAF56"
+                    }
+                    if elem.type == 1 {
+                        elem.content = "https://www.cs999.com/jpg.txt"
+                    }
+                    if elem.type == 2 {
+                        elem.content = "https://www.cs999.com/jpg.jpg"
+                    }
+                    if elem.type == 3 {
+                        elem.content = "https://www.cs999.com/amr.amr"
+                    }
+                    elems.append(elem)
+                }
+                messages.append(message)
+            }
+            list.append(conversation)
+        }
+        
+        
+        // Persist your data easily
+        try! realm.write {
+            realm.add(list)
+        }
+        
     }
 
 }
